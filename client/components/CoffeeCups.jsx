e530d4c187b629b42a83257a9fa7da5666ed8281
Nat <nat_sirisamphan@hotmail.com>
2018-09-11 08:41:52 +1200

    update coffee component

--- client/components/CoffeeCups.jsx
+++ client/components/CoffeeCups.jsx
@@ -5,18 +5,21 @@ import {Link} from 'react-router-dom'
 const CoffeeCups = props => {
     let location = Number(props.match.params.id) -1;
     let itemDb = items[location];
+
+    let name = props.match.params.name;
+    console.log(name)
+
         return (
             <React.Fragment>
                 <div className="homeCol">
-                    <h1>{itemDb.material}</h1>
-                    <p>{itemDb.description}</p>
-                    <img src={itemDb.photo}/>
+                    <h1>{name}</h1>
+                    <p>description</p>
                         <ul>
-                            <li key={itemDb.id}> Technically recyclable: {itemDb.recyclableTechnically}</li>
-                            <li key={itemDb.id}> Actually recyclable: {itemDb.recyclableActually}</li>
-                            <li key={itemDb.id}> Technically compostable: {itemDb.compostableTechnically}</li>
-                            <li key={itemDb.id}> Actually compostable: {itemDb.compostableActually}</li>
-                            <li key={itemDb.id}> Good for landfill: {itemDb.landfill}</li>
+                            <li> Technically recyclable: </li>
+                            <li> Actually recyclable: </li>
+                            <li> Technically compostable: </li>
+                            <li> Actually compostable: </li>
+                            <li> Good for landfill: </li>
                         </ul>
                     <a href="/">Home</a>
                 </div>
@@ -25,3 +28,27 @@ const CoffeeCups = props => {
 }
 
 export default CoffeeCups
+
+// const CoffeeCups = props => {
+//     let location = Number(props.match.params.id) -1;
+//     let itemDb = items[location];
+//         return (
+//             <React.Fragment>
+//                 <div className="homeCol">
+//                     <h1>{itemDb.material}</h1>
+//                     <p>{itemDb.description}</p>
+//                     <img src={itemDb.photo}/>
+//                         <ul>
+//                             <li key={itemDb.id}> Technically recyclable: {itemDb.recyclableTechnically}</li>
+//                             <li key={itemDb.id}> Actually recyclable: {itemDb.recyclableActually}</li>
+//                             <li key={itemDb.id}> Technically compostable: {itemDb.compostableTechnically}</li>
+//                             <li key={itemDb.id}> Actually compostable: {itemDb.compostableActually}</li>
+//                             <li key={itemDb.id}> Good for landfill: {itemDb.landfill}</li>
+//                         </ul>
+//                     <a href="/">Home</a>
+//                 </div>
+//             </React.Fragment>
+//         )
+// }
+
+// export default CoffeeCups
